a501f1c81a690c6c473e078ac760c89d4f010901
chamara8901 <chamara@grrow.jp>
2022-07-22 13:42:44 +0900

    add all

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,23 +1,23 @@
-module.exports={
-    title:'Grrow Blog',
-    description:'Design Your Project',
-    themeConfig:{
-        nav:[
-            {text:'Home',link:'/'},
-            {text:'docs',link:'/docs/'},
-            {text:'blog',link:'/blog/'},
-            {text:'About me',link:'https://github.com'},
-        ],
-        sidebar:{
-                '/docs/':[
-                '',
-                'frontend',
-                'backend'
-            ],
-            '/blog/':[
-                '',
-            ],
+module.exports = {
 
-        }
+    title: 'Teach English',
+    description: 'Example website powered by Vuepress and Netlify CMS',
+ 
+    themeConfig: {
+ 
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'About', link: '/about/' },
+          { text: 'Categories', link: '/categories/' }
+        ]
+       //  sidebar: [
+     //   ['/', 'Начало'],
+       // ['/about/', 'За мен'],
+       // ['/lessons/', 'Уроци']
+     // ]
+ 
+ 
+ 
     }
-}
+ }
+ 
